f6a6081e40942cf626874acdb50fab2bca1df60a
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2024-03-02 21:20:14 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,8 +155,8 @@ BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 # TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
 
 # PBRP specific build flags
-PB_TORCH_PATH := /proc/qcom_flash
-PB_TORCH_MAX_BRIGHTNESS := 1
+# PB_TORCH_PATH := /proc/qcom_flash
+# PB_TORCH_MAX_BRIGHTNESS := 1
 
 # MTK Hardware 
  BOARD_HAS_MTK_HARDWARE := true 
