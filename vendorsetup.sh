4cb88498aeb68d4e9236e15a3d3a04da75ab6f72
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2024-09-13 23:42:39 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,12 @@
 
 
 export OF_FL_PATH1="/proc/qcom_flash"
+export OF_FL_PATH2="/proc/qcom_flash"
 export OF_FLASHLIGHT_ENABLE="1"
 export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
 export OF_KEEP_FORCED_ENCRYPTION="1"
+#export FOX_DELETE_AROMAFM="1"
+export OF_USE_GREEN_LED="0"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
 export OF_MAINTAINER="omar & seif"
