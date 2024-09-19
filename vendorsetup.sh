0a96a27fde858900272f9dcf40a7618abc39b4d2
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2024-09-20 00:23:22 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,3 +12,16 @@ export OF_USE_GREEN_LED="0"
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
 export OF_USE_LEGACY_BATTERY_SERVICES="1"
 export OF_MAINTAINER="Omar & Seif"
+
+#FOX_VERSION
+#OF_SUPPORT_OZIP_DECRYPTION" [NEW]
+ #- Set this to 1 to enable support for Realme oZip decryption
+ #- do not use this unless you know what you are doing (see below)
+ #- if this is enabled, you must also set "TW_OZIP_DECRYPT_KEY"
+ #- Note: This is NOT required for realme devices that are released with android 10 or above, since realme has added 
+ #- the decryptor inside of the updater binary removing the necessity for an external decryptor. For devices that
+ #- are updated to android 10, this feature is necessary only for downgrades to pie or below.
+ #- default = 0
+
+ #OF_FBE_METADATA_MOUNT_IGNORE
+
