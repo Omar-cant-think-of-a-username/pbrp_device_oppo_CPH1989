908723f4337900627ee9dedff899d8670dfdae95
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2024-09-20 05:26:47 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,13 @@
 
 
 
-#export OF_FL_PATH1="/proc/qcom_flash"
+
 export OF_FLASHLIGHT_ENABLE="1"
+export OF_FL_PATH1="/proc/qcom_flash"
 export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
 export OF_KEEP_FORCED_ENCRYPTION="1"
-#export FOX_DELETE_AROMAFM="1"
 export OF_USE_GREEN_LED="0"
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
 export OF_USE_LEGACY_BATTERY_SERVICES="1"
@@ -14,6 +15,7 @@ export OF_MAINTAINER="Omar & Seif"
 export OF_FBE_METADATA_MOUNT_IGNORE="1"
 export FOX_VERSION="R11.1"
 export FOX_BUILD_TYPE="Unofficial"
+export OF_SCREEN_H="2340"
 
 #OF_SUPPORT_OZIP_DECRYPTION" [NEW]
  #- Set this to 1 to enable support for Realme oZip decryption
@@ -23,6 +25,9 @@ export FOX_BUILD_TYPE="Unofficial"
  #- the decryptor inside of the updater binary removing the necessity for an external decryptor. For devices that
  #- are updated to android 10, this feature is necessary only for downgrades to pie or below.
  #- default = 0
+
+ #export FOX_DELETE_AROMAFM="1"
+ #export FOX_ADVANCED_SECURITY="1"
 
  
 
