78aaa41d11aa75acea33cfaa09a62e4b56b30193
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2024-09-28 12:24:25 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -33,7 +33,7 @@ export OF_STATUS_INDENT_RIGHT=48
 export FOX_USE_SPECIFIC_MAGISK_ZIP="~/Magisk/Magisk-27.0.zip"
 export OF_DEFAULT_KEYMASTER_VERSION="3.0"
 export OF_SKIP_FBE_DECRYPTION="1"
-export FOX_SETTINGS_ROOT_DIRECTORY="/persist"
+export FOX_SETTINGS_ROOT_DIRECTORY="/persist/OFRP"
 
 #OF_SUPPORT_OZIP_DECRYPTION" [NEW]
  #- Set this to 1 to enable support for Realme oZip decryption
