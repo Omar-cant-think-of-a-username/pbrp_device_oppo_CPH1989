37391d29cec0c0ba6065dd7e77e74e8a2bb33693
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2024-11-13 22:49:08 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,13 +7,14 @@ export OF_FL_PATH1="/system/flashlight"
 export OF_USE_GREEN_LED="0"
 
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+#export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
 export OF_KEEP_FORCED_ENCRYPTION="1"
 export OF_KEEP_DM_VERITY="1"
 export OF_DONT_PATCH_ON_FRESH_INSTALLATION="1"
 # no special MIUI stuff
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
 export OF_NO_MIUI_PATCH_WARNING=1
+export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
 
 export FOX_DISABLE_APP_MANAGER="0"
@@ -24,7 +25,8 @@ export FOX_USE_NANO_EDITOR="1"
 export FOX_REMOVE_AAPT="0"
 export FOX_USE_XZ_UTILS="1"
 export FOX_USE_SED_BINARY="1"
-export OF_USE_HEXDUMP="1"
+export FOX_USE_GREP_BINARY="1"
+#export OF_USE_HEXDUMP="1" obselote
 # for oppo/realme ozip decryption
 export OF_SUPPORT_OZIP_DECRYPTION=1
 export TW_OZIP_DECRYPT_KEY="0000"
@@ -36,7 +38,7 @@ export OF_DONT_KEEP_LOG_HISTORY="1"
 export OF_FBE_METADATA_MOUNT_IGNORE="1"
 export FOX_INSTALLER_DISABLE_AUTOREBOOT="1"
 # ensure that /sdcard is bind-unmounted before f2fs data repair or format
-export OF_UNBIND_SDCARD_F2FS=1
+export OF_UNBIND_SDCARD_F2FS=1 # needs testing
 # about
 export OF_MAINTAINER="Omar"
 export FOX_VERSION="R11.1"
@@ -50,8 +52,7 @@ export OF_DEFAULT_KEYMASTER_VERSION="3.0"
 export OF_SKIP_FBE_DECRYPTION="1"
 export FOX_SETTINGS_ROOT_DIRECTORY="/persist/OFRP"
 export OF_ADVANCED_SECURITY=1
-#for testing
-export OF_DISPLAY_FORMAT_FILESYSTEMS_DEBUG_INFO=1
+
 export OF_UNMOUNT_SDCARDS_BEFORE_REBOOT=1
 
 #OF_SUPPORT_OZIP_DECRYPTION" [NEW]
@@ -68,8 +69,6 @@ export OF_UNMOUNT_SDCARDS_BEFORE_REBOOT=1
  #export FOX_DELETE_INITD_ADDON="1"
  #export OF_USE_TWRP_SAR_DETECT="1"
  #export OF_ENABLE_LPTOOLS="1"
- #FOX_USE_SED_BINARY
- #OF_USE_HEXDUMP
 # "FOX_LOCAL_CALLBACK_SCRIPT"
  #  - point to a custom "callback" script that will be executed just before creating the final recovery image
   # - eg, a script to delete some files, or add some files to the ramdisk
@@ -77,6 +76,7 @@ export OF_UNMOUNT_SDCARDS_BEFORE_REBOOT=1
 
  # ensure that /sdcard is bind-unmounted before f2fs data repair or format
 	#export OF_UNBIND_SDCARD_F2FS=1
+ #OF_NO_SPLASH_CHANGE
  
  #"OF_SPLASH_MAX_SIZE"
 #- use this to specify the maximum size (in kilobytes) of image files that can safely be used for splash images
@@ -92,6 +92,28 @@ export OF_UNMOUNT_SDCARDS_BEFORE_REBOOT=1
 #- Use with care: use only if the device/ROM has a metadata partition - and - formatting /data doesn't automatically wipe it
 #- It is up to you to verify for yourself that this is needed in the first place, and if used, that it works as expected
 #- default = 0
+
+#OF_DONT_KEEP_LOG_HISTORY"
+#TARGET_DEVICE_ALT
+#OF_REPORT_HARMLESS_MOUNT_ISSUES
+#"
+   - point to a custom temp directory for creating the zip installer
+   - ensure that this is a directory that you have write access to
+   - there is no default
+#
+"FOX_PORTS_INSTALLER" 
+   - point to a custom directory for amended/additional installer files 
+   - the contents will simply be copied over before creating the zip installer
+
+"FOX_PORTS_TMP"
+   - point to a custom temp directory for creating the zip installer
+   - ensure that this is a directory that you have write access to
+   - there is no default
+#
+"FOX_PORTS_INSTALLER" 
+   - point to a custom directory for amended/additional installer files 
+   - the contents will simply be copied over before creating the zip installer
+
 
 
 
