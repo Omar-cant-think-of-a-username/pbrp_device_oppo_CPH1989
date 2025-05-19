a1a7194a5573cd1699877b0f562eecaa832a96a7
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2025-05-19 18:58:25 +0300

    enabling decryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,10 +132,10 @@ TW_EXCLUDE_NANO := false
 TW_USE_TOOLBOX := true
 
 # Crypto
-TW_INCLUDE_CRYPTO := false
-TW_INCLUDE_CRYPTO_FBE := false
-BOARD_USES_METADATA_PARTITION := false
-TW_INCLUDE_FBE_METADATA_DECRYPT := false
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+BOARD_USES_METADATA_PARTITION := true
+#TW_INCLUDE_FBE_METADATA_DECRYPT := false
 
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_INTERNAL_STORAGE_PATH := "/data/media/0"    #needs to check on that path
