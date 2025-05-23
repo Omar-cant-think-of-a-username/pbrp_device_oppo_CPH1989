75ce9399a1ecefa40ee178c4a2c0117404a655ab
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2025-05-23 19:42:28 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,15 +7,14 @@ export OF_FL_PATH1="/system/flashlight"
 export OF_USE_GREEN_LED="0"
 
 export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-#export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
-export OF_KEEP_FORCED_ENCRYPTION="1"
-export OF_KEEP_DM_VERITY="1"
 export OF_DONT_PATCH_ON_FRESH_INSTALLATION="1"
-# no special MIUI stuff
+
+# no special MIUI/Samsung stuff
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
 export OF_NO_MIUI_PATCH_WARNING=1
 export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
 export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
+export FOX_NO_SAMSUNG_SPECIAL="1"
 
 export FOX_DISABLE_APP_MANAGER="0"
 export FOX_ENABLE_APP_MANAGER="1"
@@ -27,7 +26,7 @@ export FOX_USE_XZ_UTILS="1"
 export FOX_USE_SED_BINARY="1"
 export FOX_USE_GREP_BINARY="1"
 export FOX_USE_DATE_BINARY="1"
-#export OF_USE_HEXDUMP="1" obselote
+
 # for oppo/realme ozip decryption
 export OF_SUPPORT_OZIP_DECRYPTION=1
 #export TW_OZIP_DECRYPT_KEY="0000"
@@ -38,8 +37,7 @@ export OF_USE_MAGISKBOOT="1"
 export OF_DONT_KEEP_LOG_HISTORY="1"
 export OF_FBE_METADATA_MOUNT_IGNORE="1"
 export FOX_INSTALLER_DISABLE_AUTOREBOOT="0"
-# ensure that /sdcard is bind-unmounted before f2fs data repair or format
-#export OF_UNBIND_SDCARD_F2FS=1 # needs testing
+
 # about
 export OF_MAINTAINER="Omar"
 #export FOX_MAINTAINER_PATCH_VERSION="R11.1"
@@ -53,10 +51,11 @@ export FOX_USE_SPECIFIC_MAGISK_ZIP="~/Magisk/Magisk-27.0.zip"
 #export OF_DEFAULT_KEYMASTER_VERSION="3.0"
 #export OF_SKIP_FBE_DECRYPTION="1"
 export FOX_SETTINGS_ROOT_DIRECTORY="/persist/OFRP"
-export OF_ADVANCED_SECURITY=1
-
-export OF_UNMOUNT_SDCARDS_BEFORE_REBOOT=1
 export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/data/media/OFRP"
+
+export OF_ADVANCED_SECURITY=1
+export OF_UNMOUNT_SDCARDS_BEFORE_REBOOT=1
+
 
 #OF_SUPPORT_OZIP_DECRYPTION" [NEW]
  #- Set this to 1 to enable support for Realme oZip decryption
@@ -67,11 +66,11 @@ export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/data/media/OFRP"
  #- are updated to android 10, this feature is necessary only for downgrades to pie or below.
  #- default = 0
 
- #export FOX_DELETE_AROMAFM="1"
+ 
  #export OF_QUICK_BACKUP_LIST="/boot;/metadata"
  #export FOX_DELETE_INITD_ADDON="1"
  #export OF_USE_TWRP_SAR_DETECT="1"
- #export OF_ENABLE_LPTOOLS="1"
+
 # "FOX_LOCAL_CALLBACK_SCRIPT"
  #  - point to a custom "callback" script that will be executed just before creating the final recovery image
   # - eg, a script to delete some files, or add some files to the ramdisk
