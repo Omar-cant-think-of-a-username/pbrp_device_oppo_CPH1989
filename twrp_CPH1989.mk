0ed0868f69be6211207ca66dc6851e4de8194cff
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2025-11-29 01:01:18 +0300

    Update twrp_CPH1989.mk

--- twrp_CPH1989.mk
+++ twrp_CPH1989.mk
@@ -10,9 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Twrp stuff.
-#$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 # for pbrp
- $(call inherit-product, vendor/pb/config/common.mk)
+#$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from CPH2001 device
 # $(call inherit-product, device/oppo/CPH1989/device.mk)
