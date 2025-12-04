562487cf4433cb5050e68791bad86454f5844c12
Omar? Omar! <158614055+Omar-cant-think-of-a-username@users.noreply.github.com>
2025-12-04 21:28:31 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,17 +169,17 @@ SHRP_EXTERNAL := /external_sd
 SHRP_INTERNAL := /sdcard
 SHRP_OTG := /usb-otg
 SHRP_FLASH := 1
-SHRP_FONP_1 := /system/flashlight/brightness
+SHRP_FONP_1 := "/system/flashlight/brightness"
 SHRP_FLASH_MAX_BRIGHTNESS := 1
-SHRP_REC := </dev/block/by-name/recovery>
+SHRP_REC := "/dev/block/by-name/recovery"
 SHRP_EXPRESS := true
-SHRP_DEVICE_TYPE := <A/B>
-SHRP_REC_TYPE := <Normal>
+SHRP_DEVICE_TYPE := "A/B"
+SHRP_REC_TYPE := "Normal"
 
 
 SHRP_NOTCH := false
-SHRP_STATUSBAR_RIGHT_PADDING := 40
-SHRP_STATUSBAR_LEFT_PADDING := 40
+SHRP_STATUSBAR_RIGHT_PADDING := 42
+SHRP_STATUSBAR_LEFT_PADDING := 42
 
 # MTK Hardware 
  BOARD_HAS_MTK_HARDWARE := true 
