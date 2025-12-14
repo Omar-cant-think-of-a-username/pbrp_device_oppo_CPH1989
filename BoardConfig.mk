02debb1b78f97f9b801d1cc8bd69ec4238217f28
Omar? Omar! <158614055+real-omar@users.noreply.github.com>
2025-12-14 19:36:18 +0200

    trying to fix keymaster 3 service

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,6 +169,17 @@ TW_DEFAULT_LANGUAGE := en
 TW_INCLUDE_LIBRESETPROP := true
 TW_INCLUDE_RESETPROP := true
 
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libpuresoftkeymasterdevice \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so \
+
 
 # PBRP specific build flags
 PB_TORCH_PATH := "/proc/qcom_flash"
