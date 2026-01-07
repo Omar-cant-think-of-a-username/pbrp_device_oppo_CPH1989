8631976f2e96ea5a57e940d2983c154483795f3a
Omar? Omar! <158614055+real-omar@users.noreply.github.com>
2026-01-07 16:33:27 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,17 +172,15 @@ TW_INCLUDE_RESETPROP := true
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     libpuresoftkeymasterdevice
-#    libsoftkeymasterdevice \
-#    libkeymaster_portable \
-#    ashmemd_aidl_interface-cpp \
-#    libashmemd_client
+    libsoftkeymasterdevice \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-#    $(TARGET_OUT_SHARED_LIBRARIES)/libsoftkeymasterdevice.so \
-#    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_portable.so \
-#    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
-#    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libsoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
 
 
 # PBRP specific build flags
