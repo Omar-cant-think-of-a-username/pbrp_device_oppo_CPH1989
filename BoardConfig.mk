f8c2b2332d27ed464557667492322887c12a59a7
Omar? Omar! <158614055+real-omar@users.noreply.github.com>
2026-01-07 18:45:48 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,12 +171,12 @@ TW_INCLUDE_RESETPROP := true
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
-    libpuresoftkeymasterdevice
+    libpuresoftkeymasterdevice \
     ashmemd_aidl_interface-cpp \
     libashmemd_client
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/ashmemd_aidl_interface-cpp.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libashmemd_client.so
 
