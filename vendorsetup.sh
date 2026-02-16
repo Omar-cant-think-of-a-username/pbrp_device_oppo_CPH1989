5e8bcf0b962c99fea15cb736a19ca27b33a9930d
Omar? Omar! <158614055+real-omar@users.noreply.github.com>
2026-02-16 13:42:51 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,24 +1,44 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2026 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
 
-
+#set -o xtrace
 FDEVICE="CPH1989"
 
 fox_get_target_device() {
-	export script_path="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-	if echo "$script_path" | grep -q "$FDEVICE"; then
-		FOX_BUILD_DEVICE="$FDEVICE"
-	elif echo "$0" | grep -q "$FDEVICE"; then
-		FOX_BUILD_DEVICE="$FDEVICE"
-	fi
+local chkdev
+  if echo "$BASH_SOURCE" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif set | grep BASH_ARGV | grep -w \"$FDEVICE\"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif echo "${BASH_SOURCE[0]}" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  elif echo "$0" | grep -q "$FDEVICE"; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+  fi
 }
 
 if [ -z "$FOX_BUILD_DEVICE" ]; then
 	fox_get_target_device
 fi
 
-if [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	echo "Detected build device: $FOX_BUILD_DEVICE"
-
-
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
 # Flashlight
 export OF_FLASHLIGHT_ENABLE="1"
@@ -42,15 +62,15 @@ export FOX_USE_XZ_UTILS="1"
 # for oppo/realme ozip decryption
 export OF_SUPPORT_OZIP_DECRYPTION=1
 
+
 export OF_DONT_KEEP_LOG_HISTORY="1"
 export OF_FBE_METADATA_MOUNT_IGNORE="1"
 
 # about
 export OF_MAINTAINER="Omar"
-export FOX_BUILD_TYPE="Beta"
 export TARGET_DEVICE_ALT="OP4C4B"
 #export FOX_MAINTAINER_PATCH_VERSION="04"
-#export FOX_VARIANT="FBE"
+#export FOX_VARIANT="Default"
 
 export OF_SCREEN_H="2340"
 export OF_STATUS_INDENT_LEFT="73"
@@ -59,19 +79,16 @@ export FOX_SETTINGS_ROOT_DIRECTORY="/persist/OFRP"
 export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/data/media/OFRP"
 export OF_QUICK_BACKUP_LIST="/boot;/data;"
 export OF_DEFAULT_TIMEZONE="WET-2"
-#magisk
-export FOX_USE_SPECIFIC_MAGISK_ZIP="$script_path/prebuilt/Magisk-v30.6.zip"
 
 export OF_ADVANCED_SECURITY=1
 
-else
-	echo "I: vendorsetup.sh skipped; device mismatch or environment issue."
 fi
-
 
 
 #export OF_FIX_DECRYPTION_ON_DATA_MEDIA="1"
 #"OF_NO_RELOAD_AFTER_DECRYPTION"
 #OF_SKIP_FBE_DECRYPTION_SDKVERSION=31
 #OF_SPLASH_MAX_SIZE
+
+#
 
