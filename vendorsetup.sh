ef22c32d744ab0d7a9c971f6693ce94e755c4996
Omar? Omar! <158614055+real-omar@users.noreply.github.com>
2026-02-18 19:48:40 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -58,9 +58,10 @@ export FOX_USE_XZ_UTILS="1"
 #export FOX_USE_XZ_UTILS="0"
 export FOX_REMOVE_AAPT="1"
 export FOX_DELETE_INITD_ADDON="1"
+export FOX_REMOVE_BASH="1"
 #export FOX_USE_NANO_EDITOR="0"
 #export FOX_EXCLUDE_NANO_EDITOR="1"
-export FOX_DRASTIC_SIZE_REDUCTION="1"
+#export FOX_DRASTIC_SIZE_REDUCTION="1"
 
 # for oppo/realme ozip decryption
 export OF_SUPPORT_OZIP_DECRYPTION=1
